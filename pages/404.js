d22149dca4c87e77ef683db35ae36e9efb1833dc
JT <joshuaharrison1731@gmail.com>
2021-07-06 15:26:49 -0400

    Fix fonts #69

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function Custom404() {
   return (
     <div className="container m-auto placehold-player bg-gray-700 border-2 border-gray-500">
-      <h1 className="text-purple-500 font-bold mt-2 align-middle text-2xl flex justify-around load">
+      <h1 className="text-purple-500 font-bold mt-8 align-middle text-2xl flex justify-around load">
         Watch Twitch clips forever.
       </h1>
       <img
